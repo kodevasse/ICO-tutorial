150d2bb5794cac18daf2e1440d12a647e3a88842
Kode Vasse <kodevasse@outlook.com>
2022-01-31 20:05:04 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x9De0f3dB1B1a2184A3F135E0CB867a6f58e4945F";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
